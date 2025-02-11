0914cf7bcf9755f646efd0554efd3b724123981a
f47iw4 <fgrhanny@gmail.com>
2025-02-11 16:13:06 +0100

    Add JSON and index.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+
+
 fetch ('data/productos.json')
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data.productos);
+    })
+    .catch(error=>console.error("Error fetching JSON: ", error));
+
